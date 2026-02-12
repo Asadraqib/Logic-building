2887f0b85bfc4f25af844222c65a67fd68d1d63f
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-02-12 17:37:26 +0530

    Implement value swapping using a third variable

--- C Programming/value_swap_3rd_variable.cpp
+++ C Programming/value_swap_3rd_variable.cpp
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+int main(){
+	int a = 10, b = 5, c = 0;
+	printf("Before swapping - a = %d and b = %d", a, b);
+	c = a;
+	a = b;
+	b = c;
+	printf("After swapping - a = %d and b = %d", a, b);
+	return 0;
+}
