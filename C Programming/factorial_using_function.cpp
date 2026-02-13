e6c9273c646d60f8d8fe6e3f57bc5621d72e5812
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-02-13 20:31:00 +0530

    Implement factorial function in C

--- C Programming/factorial_using_function.cpp
+++ C Programming/factorial_using_function.cpp
@@ -1,1 +1,16 @@
+#include<stdio.h>
+int fact(int num){
+	int i,f=1;
+	for (i=1;i<=num;i++){
+		f=f*i;
+	}
+	return f;
+}
 
+int main(){
+	int num;
+	printf("Enter any number: ");
+	scanf("%d",&num);
+	printf("factorial of %d is %d",num,fact(num));
+	return 0;
+}
