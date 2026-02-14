dc0a39d02b3eaf56e244140862c8c93ca24277fa
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-02-14 20:33:46 +0530

    Update factorial_using _recursion.cpp

--- C Programming/factorial_using _recursion.cpp
+++ C Programming/factorial_using _recursion.cpp
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int fact(int n){
+	if(n==0)
+		return 1;
+	else
+		return (n*fact(n-1));
+}
+int main(){
+	int num;
+	printf("Enter a number to find its factorial: ");
+	scanf("%d",&num);
+	printf("The factorial of %d is %d.",num,fact(num));
+	return 0;
+}
