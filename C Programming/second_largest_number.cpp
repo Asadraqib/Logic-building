5eb3076fc964034d85f6c4af07088771014c64d9
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-02-16 20:23:09 +0530

    Update second_largest_number.cpp

--- C Programming/second_largest_number.cpp
+++ C Programming/second_largest_number.cpp
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+int main(){
+	int n = 5,largest,secondlargest,arr[] = {10,25,55,40,33};
+	largest = secondlargest = arr[0];
+	for (int i = 1; i<n;i++){
+		if(arr[i]>largest){
+			secondlargest = largest;
+			largest = arr[i];
+		}
+		else if(arr[i]>secondlargest && arr[i] != largest){
+		
+			secondlargest = arr[i];
+		}
+	}
+	printf("Second largest element = %d",secondlargest);
+	return 0;
+}
