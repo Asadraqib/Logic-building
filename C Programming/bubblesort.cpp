6f053d5485bfc23d30414861ea5244e81440b3ff
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-02-17 19:38:13 +0530

    Implement bubble sort algorithm in C

--- C Programming/bubblesort.cpp
+++ C Programming/bubblesort.cpp
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+int main(){
+	int arr[]={5,1,4,2,8};
+	int n = 5;
+	int i , j , temp;
+	for (i = 0; i<n-1; i++){
+		for (j = 0; j<n-i-1; j++){
+			if (arr[j]>arr[j+1]){
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+	printf("Sorted Array:\n");
+	for (i=0;i<n;i++){
+		printf("%d ",arr[i]);
+	}
+	return 0;
+}
