b3404a11abb50defd22977d2a732c5286fe2287b
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-02-22 14:16:14 +0530

    Implement employee struct with file handling

--- C Programming/struct_strudent_and_filehandling.c
+++ C Programming/struct_strudent_and_filehandling.c
@@ -1,1 +1,41 @@
+#include<stdio.h>
+struct Employee{
+	int id;
+	char name[30];
+	char department[20];
+	float salary;
+};
 
+int main(){
+	struct Employee emp;
+	FILE *fp;
+	
+	emp.id = 101;
+	sprintf(emp.name,"Rakesh Sharma");
+	sprintf(emp.department,"Finance");
+	emp.salary = 35000.50;
+	
+	fp = fopen("employee.txt","w");
+	if (fp==NULL){
+		printf("File cannot be opened\n");
+		return 1;
+	}
+	fprintf(fp,"%d %s %s %.2f\n",emp.id,emp.name,emp.department,emp.salary);
+	fclose(fp);
+	
+	fp = fopen("employee.txt","r");
+	if (fp==NULL){
+		printf("File cannot be opened\n");
+		return 1;
+	}
+	fscanf(fp,"%d %s %s %.2f\n",&emp.id,emp.name,emp.department,&emp.salary);
+	fclose(fp);
+	
+	printf("Employee Details:\n");
+	printf("ID: %d\n",emp.id);
+	printf("Name: %s\n",emp.name);
+	printf("Department: %s\n",emp.department);
+	printf("Salary: %.2f",emp.salary);
+	
+	return 0;
+}
