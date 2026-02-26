1f088e70a117cfaddee79788b1177b74654b6346
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-02-26 20:36:50 +0530

    Update fibonacci_series_recursive.c

--- C Programming/fibonacci_series_recursive.c
+++ C Programming/fibonacci_series_recursive.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int fibo(int n){
+  if(n<=1)
+  return n;
+  return (fibo(n-1)+fibo(n-2));
+}
+int main(){
+  int term,i;
+  printf("Enter the nth term of the series: ");
+  scanf("%d",&term);
+  for(i=0;i<term;i++)
+    printf("%d ",fibo(i));
+  return 0;
+}
