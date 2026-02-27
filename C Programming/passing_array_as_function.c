d7efa5fad5b64ab3049ad7e1b3ad47f8caef4a92
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-02-27 17:05:26 +0530

    Update passing_array_as_function.c

--- C Programming/passing_array_as_function.c
+++ C Programming/passing_array_as_function.c
@@ -1,1 +1,16 @@
-
+//Passing array to a function
+#include <stdio.h>
+// Function to display array
+void display(int arr[], int n) {
+  int i;
+  for (i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+}
+int main() {
+  int a[] = {10, 20, 30, 40, 50};
+  int n = 5;
+  printf("Array elements:\n");
+  display(a, n);
+  return 0;
+}
