bb5271069c4a91f5d662dc99783a480aa4eff8ce
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-03-01 13:38:32 +0530

    Implement structure and function to display employee data

--- C Programming/passing_structure_as_function.c
+++ C Programming/passing_structure_as_function.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+// Define structure
+struct Employee {
+int id;
+char name[30];
+float salary;
+};
+// Function to display structure data
+void display(struct Employee e) {
+printf("ID: %d\n", e.id);
+printf("Name: %s\n", e.name);
+printf("Salary: %.2f\n", e.salary);
+}
+int main() {
+struct Employee emp = {101, "Ankit Kumar", 55000.00};
+display(emp);
+return 0;
+}
