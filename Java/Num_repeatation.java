ee035e5c4171b08dd49b38faa8871710ded6d87f
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-04-01 21:21:23 +0530

    Update Num_repeatation.java

--- Java/Num_repeatation.java
+++ Java/Num_repeatation.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class Num_repeatation {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter numbers: ");
+        int num = input.nextInt();
+        System.out.print("Enter number to find its occurrence: ");
+        int n = input.nextInt();
+        int rem,count=0;
+        while (num > 0){
+            rem = num % 10;
+            if (rem == n){
+                count++;
+            }
+            num = num / 10;
+        }
+        System.out.print("Number "+ n +" occured "+ count +" times");
+    }
+}
 
