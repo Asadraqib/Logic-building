5579800518d103f90b363827a7652aee203f7dad
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-04-01 21:20:22 +0530

    Update Fibonacci_series.java

--- Java/Fibonacci_series.java
+++ Java/Fibonacci_series.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class Fibonacci_series {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter nth position in the series: ");
+        int n = input.nextInt();
+        int a = 0;
+        int c = 1;
+        int count = 2;
+        while (count <= n){
+            int temp = c;
+            c = c + a;
+            a = temp;
+            count++;
+        }
+        System.out.println("In nth position of Fibonacci series is: " + c);
+    }
+}
