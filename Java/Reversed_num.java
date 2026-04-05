3bf527ce88b911bd2dc093813d494759d27fb872
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-04-05 21:38:51 +0530

    Update Reversed_num.java

--- Java/Reversed_num.java
+++ Java/Reversed_num.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class Reversed_num {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter numbers: ");
+        int num = input.nextInt();
+        int rem,rev=0;
+        while (num > 0){
+            rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+        }
+        System.out.print("Reversed number is: " + rev);
+    }
+}
 
