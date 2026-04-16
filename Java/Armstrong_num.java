7d5949e6220c2a855e9310d2066356b5dcaf13e6
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-04-16 21:32:53 +0530

    Update Armstrong_num.java

--- Java/Armstrong_num.java
+++ Java/Armstrong_num.java
@@ -1,1 +1,26 @@
+public class Armstrong_num {
+    public Armstrong_num() {
+    }
+
+    static void main() {
+        for(int i = 0; i < 1000; ++i) {
+            if (isArmstrong(i)) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+
+    static boolean isArmstrong(int num) {
+        int temp = num;
+
+        int ans;
+        for(ans = 0; num > 0; num /= 10) {
+            int rem = num % 10;
+            ans += rem * rem * rem;
+        }
+
+        return ans == temp;
+    }
+}
 
